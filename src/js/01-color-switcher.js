d6982d70d2cbfdcbb5d44896d41b12fcfa8bce81
Alina <alina.bilchich@gmail.com>
2022-10-28 20:35:17 +0300

    task 1 is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector("body");
+let interval = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+startBtn.addEventListener("click", () => {
+  interval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    startBtn.setAttribute("disabled", true);
+    stopBtn.removeAttribute("disabled");
+});
+
+stopBtn.disabled = true;
+stopBtn.addEventListener("click", () => {
+    startBtn.removeAttribute("disabled");
+    stopBtn.setAttribute("disabled", true)
+
+    clearInterval(interval);
+});
+
+// function clickOnStart() {
+//     startBtn.disabled = true;
+//     stopBtn.disableButton = false;
+// }
+
+// function clickOnStop() {
+//     stopBtn.disableButton = true;
+//      startBtn.disabled = false;
+// }
+
+// startBtn.addEventListener('click', clickOnStart);
+// stopBtn.addEventListener('click', clickOnStop);
