1edcf52f00a8a5c57fcfaa22ef5592406bd5cbf7
alina-tuan <105650183+alina-tuan@users.noreply.github.com>
2022-10-28 17:35:39 +0000

    Deploying to gh-pages from @ alina-tuan/goit-js-hw-09@d6982d70d2cbfdcbb5d44896d41b12fcfa8bce81 🚀

---
+++ 01-color-switcher.02a81b1b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),r=document.querySelector("body");let d=null;e.addEventListener("click",(()=>{d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.setAttribute("disabled",!0),t.removeAttribute("disabled")})),t.disabled=!0,t.addEventListener("click",(()=>{e.removeAttribute("disabled"),t.setAttribute("disabled",!0),clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.02a81b1b.js.map
